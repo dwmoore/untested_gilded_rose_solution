25b92fcc49c3e9bf41435deefc12534d14d2e57a
DAVID MOORE <khundawg@gmail.com>
2015-02-18 22:53:15 -0500

    Add tests for Sulfuras and Backstage passes
    Continued with four phase testing pattern to confirm outcomes based on job specs and sample data.

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -101,23 +101,110 @@ describe "#update_quality" do
 
       context "when quality reaches 0" do
         it "no longer decrements quality" do
-          vest = [Item.new("+5 Dexterity Vest", 10, 20)]
+          elixir = [Item.new("Elixir of the Mongoose", 5, 7)]
           25.times do
-            update_quality(vest)
+            update_quality(elixir)
           end
 
-          expect(vest.first.quality).to eq 0
-          expect(vest.first.sell_in).to eq(-15)
+          expect(elixir.first.quality).to eq 0
+          expect(elixir.first.sell_in).to eq(-20)
         end
       end
     end
   end
 
   context "when given Sulfuras, Hand of Ragnaros" do
+    it "maintains quality regardless of time" do
+      sulfuras = [Item.new("Sulfuras, Hand of Ragnaros", 0, 80)]
+      rand(15).times do
+        update_quality(sulfuras)
+      end
 
+      expect(sulfuras.first.quality).to eq 80
+      expect(sulfuras.first.sell_in).to eq 0
+    end
   end
 
   context "when given Backstage passes" do
+    context "when sell_in is over 10" do
+      it "increments quality by one" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        3.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.quality).to eq 23
+      end
+
+      it "decrements sell_in by one" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        3.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.sell_in).to eq 12
+      end
+    end
+
+    context "when sell_in is 10 or less" do
+      it "increments quality by two" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        7.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.quality).to eq 29
+      end
+
+      it "decrements sell_in by one" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        7.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.sell_in).to eq 8
+      end
+    end
+
+    context "when sell_in is 5 or less" do
+      it "increments quality by three" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        12.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.quality).to eq 41
+      end
+
+      it "decrements sell_in by one" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        12.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.sell_in).to eq 3
+      end
+    end
+
+    context "when sell_in is 0" do
+      it "sets the quality to 0" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        16.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.quality).to eq 0
+      end
+
+      it "decrements sell_in by one" do
+        pass = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        16.times do
+          update_quality(pass)
+        end
+
+        expect(pass.first.sell_in).to eq(-1)
+      end
+    end
 
   end
 
